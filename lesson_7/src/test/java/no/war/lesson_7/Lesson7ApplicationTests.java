2fb12041961a26db99aa2487135884b562f0d50b
shuricans <shuricans@gmail.com>
2022-08-27 12:45:51 +0300

    init lesson 7

---
+++ lesson_7/src/test/java/no/war/lesson_7/Lesson7ApplicationTests.java
@@ -0,0 +1,13 @@
+package no.war.lesson_7;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lesson7ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
